79b5b41f988356aa78de44da6e6db30784ce6b67
Alex Ruiz <alexrenardrr@gmail.com>
2022-05-05 13:13:58 +0200

    hasta minut 11:29

--- myproject/main.js
+++ myproject/main.js
@@ -1,6 +1,5 @@
 import './style.css'
 import * as THREE from 'three';
-
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
 
 const scene = new THREE.Scene();
@@ -32,6 +31,22 @@ const gridHelper = new THREE.GridHelper(200, 50)
 scene.add(lightHelper, gridHelper)
 
 const controls = new OrbitControls(camera, renderer.domElement);
+
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial({color: 0xffffff})
+  const star = new THREE.Mesh(geometry, material);
+
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+
+  star.position.set(x, y, z);
+  scene.add(star)
+}
+
+Array(200).fill().forEach(addStar)
+
+const spaceTexture = new THREE.TextureLoader().load('space.jpg')
+scene.background = spaceTexture;
 
 function animate() {
   requestAnimationFrame(animate);
